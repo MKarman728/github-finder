7509996bf293ba747f7dd72ea2f02fa938292fec
Mark Karman <mkarman08@gmail.com>
2023-11-15 15:46:19 -0800

    added Home, About, and NotFound pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
@@ -8,7 +11,14 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="text-white mx-auto px-3 pb-12">Content</main>
+        <main className="text-white mx-auto px-3 pb-12">
+          <Routes>
+            <Route path='/' element = {<Home />}></Route>
+            <Route path='/about' element = {<About />}></Route>
+            <Route path='/notfound' element = {<NotFound />}></Route>
+            <Route path='/*' element = {<NotFound />}></Route>
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
