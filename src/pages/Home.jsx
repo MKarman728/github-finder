2eed7a3e8478cf732efad8aa240c864bca111fc1
Mark Karman <mkarman08@gmail.com>
2023-11-16 14:46:00 -0800

    fixed github token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
+  const test = `${process.env.REACT_APP_GITHUB_TOKEN}`
+  console.log(test);
   return (
     <div>
-        <h1 className="text-6xl">Welcome</h1>
-        {process.env.REACT_APP_GITHUB_TOKEN}
+        <UserResults />
+
+        
     </div>
   )
 }
