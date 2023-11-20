6db89f6752465287155640bd9e95806ee599de40
Mark Karman <mkarman08@gmail.com>
2023-11-20 14:38:18 -0800

    removing .env key

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import UserResults from '../components/users/UserResults'
 
 function Home() {
-  const test = `${process.env.REACT_APP_GITHUB_TOKEN}`
-  console.log(test);
+
   return (
     <>
         <UserResults />
